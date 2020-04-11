7ef8fc39b4116f7eda6a869a178a22b9b18e66db
davidauza.engineer <davidauza01@gmail.com>
2020-04-10 23:27:24 -0500

    Set up home title
    Added a test for the home's title
    
    Cofigured the home title as Home | Prismay

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  render_views
+  let(:base_title) { ' | Prismay' }
+
+  context 'when visiting Home Page' do
+    before do
+      get :home
+    end
+
+    it 'successfully gets home' do
+      expect(response.code).to eq '200'
+    end
+
+    it 'has the right title' do
+      expect(response.body).to have_title 'Home' + base_title
+    end
+  end
+end
